630e95253eda6f4c171e5917cb2f6aba2fae2747
ne0tzigan <83465929+ne0tzigan@users.noreply.github.com>
2021-12-02 17:09:06 +0300

    Add files via upload

---
+++ WebApplication1/D&D_CharList/Character.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DND_Charlist
+{
+    public class Character
+    {
+        Race race;
+        Class[] classes;
+        short int[] levels;
+        Armor[] Armor { get; set; }
+        Weapon[] Weapons { get; set; }
+        Ability[] Abilities { get; set; }
+    }
+}
